e4e6444c00f47f233e27b5acda88cb35c4d17fa9
PRIZEOX <oganisyanararat1@gmail.com>
2023-08-10 16:43:06 +0400

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text className="text-center text-green-500">HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
